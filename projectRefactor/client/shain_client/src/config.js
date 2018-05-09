6e5e0a0bf072b3897396817b351738f817cd9adf
Juan Carlos Estrada Alvarez <juank9712@gmail.com>
2018-05-09 01:57:11 -0500

    sdsd

--- projectRefactor/client/shain_client/src/config.js
+++ projectRefactor/client/shain_client/src/config.js
@@ -4,7 +4,7 @@ const normalize = require('normalize-path');
 
 export default {
 
-    apiHost: 'http://pi2shain.dis.eafit.edu.co' || process.env.API_HOST,
+    apiHost: 'pi2shain.dis.eafit.edu.co' || process.env.API_HOST,
     ethNetwork: 'http://pi2shain.dis.eafit.edu.co/bcnet' || process.env.ETH_NETWORK,
     rootPath: normalize(process.cwd()),
     app: {
