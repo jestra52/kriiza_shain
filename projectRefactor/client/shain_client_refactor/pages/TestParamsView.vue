ff7379c6311b532840fdd56de02643c04414df9f
NoNameOriginal <clondo46@eafit.edu.co>
2018-05-22 10:40:09 -0500

    adsf

--- projectRefactor/client/shain_client_refactor/pages/TestParamsView.vue
+++ projectRefactor/client/shain_client_refactor/pages/TestParamsView.vue
@@ -18,31 +18,9 @@
                 <v-flex xs12 lg6>
                     <v-card light>
                         <v-card-text>
-                            {{ params }}
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-
-                <v-flex xs12 lg6>
-                    <v-card light>
-                        <v-card-text>
-                            {{ params }}
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-
-                <v-flex xs12 lg6>
-                    <v-card light>
-                        <v-card-text>
-                            {{ params }}
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-
-                <v-flex xs12 lg6>
-                    <v-card light>
-                        <v-card-text>
-                            {{ params }}
+                            <p>Recibido de la cuenta {{hola[0]}}</p>
+                            <p>Enviado a la cuenta: {{hola[1]}}</p>
+                            <p>Se envio: {{hola[2]}}KG de cafe</p>
                         </v-card-text>
                     </v-card>
                 </v-flex>
@@ -61,13 +39,44 @@
 </template>
 
 <script>
+import config from '../config';
+
+const apiURL = 'http://' + config.apiHost;
+console.log('ETH NETWORK', config.ethNetwork);
+
+const Web3 = require("web3");
+const web3 = new Web3();
+
+//import contractVerde from '../output/CafeVerde.json';
+//import contractTostado from '../output/CafeTostado.json';
+
+const contractVerde = require("../output/CafeVerde.json");
+const contractTostado = require("../output/CafeTostado.json");
+web3.setProvider(new web3.providers.HttpProvider(config.ethNetwork));
+const abiDecoder = require('abi-decoder');
+const testABI = require("../output/CafeVerde.json");
+abiDecoder.addABI(testABI.abi);
 
 export default {
     data () {
         return {
-            params: this.$route.params
+            params: this.$route.params,
+            hola: []
         }
-    }
+    },
+    methods:{
+         begin: function() {
+             var transaction = web3.eth.getTransaction(this.$data.params.param);
+             const testData = transaction.input;
+             const decodedData = abiDecoder.decodeMethod(testData);  
+             this.$data.hola.push(transaction.from);
+             this.$data.hola.push(transaction.to);
+             this.$data.hola.push(decodedData.params[1].value)
+         }
+    },
+    beforeMount() {
+    this.begin();
+  }
 }
 
 </script>
